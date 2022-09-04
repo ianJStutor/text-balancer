ed10925a17f0f7849d70a515f99e43aa40c162ce
Ian Marshall <ian.js.tutor@gmail.com>
2022-09-03 21:34:41 -0400

    minor changes and debugging

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 import { balanceText } from "../../text-balancer.js";
 
+function balanceLinesOfText() {
+    const h3 = document.querySelector("header h3");
+    console.log(h3.getBoundingClientRect());
+    balanceText(h3);
+    console.log(h3.getBoundingClientRect());
+}
+balanceLinesOfText();
+window.addEventListener("resize", balanceLinesOfText);
+
 const input = document.querySelector("main input");
 const output = document.querySelector("main output");
 
--- text-balancer.js
+++ text-balancer.js
@@ -1,4 +1,5 @@
 function balanceText(elementWithText) {
+    elementWithText.removeAttribute("style");
     elementWithText.style.boxSizing = "border-box";
 	const { width, height } = elementWithText.getBoundingClientRect(),
 		style = window.getComputedStyle(elementWithText),
@@ -6,9 +7,13 @@ function balanceText(elementWithText) {
         paddingRight = Number(style.getPropertyValue("padding-right").replace("px", "")) || 0,
         paddingBottom = Number(style.getPropertyValue("padding-bottom").replace("px", "")) || 0,
         paddingLeft = Number(style.getPropertyValue("padding-left").replace("px", "")) || 0,
-		[maxPadL, maxPadR] = getMaxPadding(elementWithText, {width, height: height - paddingTop - paddingBottom, paddingLeft, paddingRight});
-	elementWithText.style.paddingLeft = maxPadL + "px";
-	elementWithText.style.paddingRight = maxPadR + "px";
+		borderTop = Number(style.getPropertyValue("border-top-width").replace("px", "")) || 0,
+        borderRight = Number(style.getPropertyValue("border-right-width").replace("px", "")) || 0,
+        borderBottom = Number(style.getPropertyValue("border-bottom-width").replace("px", "")) || 0,
+        borderLeft = Number(style.getPropertyValue("border-left-width").replace("px", "")) || 0,
+		[maxPadL, maxPadR] = getMaxPadding(elementWithText, {width: width - borderLeft - borderRight, height: height - paddingTop - paddingBottom - borderTop - borderBottom, paddingLeft, paddingRight});
+    if (maxPadL > paddingLeft) elementWithText.style.paddingLeft = maxPadL + "px";
+	if (maxPadR > paddingRight) elementWithText.style.paddingRight = maxPadR + "px";
 }
 
 function getMaxPadding(el, {width, height, paddingLeft, paddingRight} = {}) {
@@ -17,16 +22,17 @@ function getMaxPadding(el, {width, height, paddingLeft, paddingRight} = {}) {
 	div.style.left = "-9999px";
 	div.style.width = width + "px";
 	div.style.boxSizing = "border-box";
-	div.dataset.padL = paddingLeft;
-	div.dataset.padR = paddingRight;
+	div.dataset.padL = paddingLeft ?? 0;
+	div.dataset.padR = paddingRight ?? 0;
 	div.innerHTML = el.innerHTML;
 	document.body.append(div);
 	while(getHeight() <= height && div.offsetWidth <= width){
 		div.dataset.padL++;
 		div.dataset.padR++;
 	}
-	const padL = div.dataset.padL - 1,
-		  padR = div.dataset.padR - 1;
+    const isSingleLine = div.offsetWidth > width ? true : false,
+          padL = isSingleLine ? paddingLeft ?? 0 : div.dataset.padL - 1,
+		  padR = isSingleLine ? paddingRight ?? 0 : div.dataset.padR - 1;
 	div.remove();
 	return [padL, padR];
 
